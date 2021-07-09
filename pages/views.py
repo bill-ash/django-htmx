e420973895c9c0586c454e28751830dd26f8233c
Bill Ash <bill@overco.net>
2021-07-08 23:21:25 -0400

    Add a simple form and text.

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Contact 
+from .forms import ContactForm
+ 
 
 def home_view(request): 
-    context = {}
-
-    return render(request, 'pages/home.html', context)
+    if request.POST:
+        form = ContactForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            return render(request, 'pages/home.html')
+    else: 
+        return render(request, 'pages/home.html')
